89268ed44b7ba6cc25bfbb49cfe9995804c82768
Lukie Kang <lkang@bink.com>
2023-05-31 22:39:18 +0100

    feat: placeholder join game form

---
+++ src/app/games/[id]/JoinGameForm/JoinGameForm.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { FormEvent, useEffect, useState } from "react"
+import { useRouter } from 'next/navigation';
+import { supabase } from '@/lib/supabaseClient'
+import {Player} from "@/types"
+
+export default function JoinGameForm({gameId}: {gameId: number}){
+  const router = useRouter()
+  const [formState, setFormState] = useState({
+    name: "",
+  })
+  const [playerData, setPlayerData] = useState<Player[]>()
+  
+  useEffect(() => { // Set Local storage and redirect if data has been fetched
+    if (playerData) {
+      localStorage.clear()
+      localStorage.setItem('player_id', playerData[0]?.id.toLocaleString())
+      localStorage.setItem('game_id', gameId.toLocaleString())
+      router.push(`/games/${gameId}`)
+    }
+  }, [gameId, playerData, router])
+
+  const handleJoinGameClick = async (e:FormEvent) => {
+    e.preventDefault()
+  
+    const { error, data: playerData } = await supabase
+      .from('players')
+      .insert({ name: formState.name, game_id: gameId})
+      .select()
+    
+     playerData && setPlayerData(playerData) 
+    }
+
+  return (
+    <form className="flex flex-col bg-teal-800" onSubmit={handleJoinGameClick}>
+      <label>Your Name (what other team members will see)</label>
+      <input className='bg-gray-400' type="text" value={formState.name} onChange={e => setFormState({...formState, name: e.target.value})} />
+      <button className="bg-green-400 text-white p-4 rounded-lg shadow-lg">Join</button>
+    </form>
+  )
+}
