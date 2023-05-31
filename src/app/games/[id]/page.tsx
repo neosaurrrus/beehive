77a026eb62a4b16a1dd074a01dffca8d9ed6a02b
Lukie Kang <lkang@bink.com>
2023-05-31 22:38:47 +0100

    change: move jsx out of page into components

--- src/app/games/[id]/page.tsx
+++ src/app/games/[id]/page.tsx
@@ -1,34 +1,30 @@
 import { supabase } from './../../../lib/supabaseClient'
-import {Heading1, Heading2, Paragraph} from "@/components/Text/Text"
-
-interface Game {
-  name: string
-  admin: string
-  id: number
-}
+import GameContainer from './GameContainer/GameContainer'
+import {Game} from "@/types"
 
 export default async function Game({ params }: { params: { id: string } }) {
+  const game = await getGame(params.id)
+  const players = await getPlayers(params.id)
 
-  const game = await getGame(params.id) 
-  const { name, admin} = game
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <Heading1>Game Name:{name}</Heading1>
-     <section>
-     <Heading2>The Admin: {admin}</Heading2>
-      <Paragraph>I am: </Paragraph>
-     </section>
-    </main>
-  )
+  if (game && players) {
+    return <GameContainer game={game} players={players} />
+  } else { 
+    return <div>Game not found</div>
+  }
 }
 
 export async function getGame(id: string): Promise<Game> {
-  let {data} = await supabase.from('games')
+  const {data} = await supabase.from('games')
      .select()
      .eq('id', Number(id))
  if (!data) {
    throw new Error('Failed to fetch data')
  }
-  return data[0] as Game
+  return data[0] 
+}
+
+export async function getPlayers(gameId: string) {
+  const {data} = await supabase.from('players').select('*').eq('game_id', gameId)
+
+  return data
 }
