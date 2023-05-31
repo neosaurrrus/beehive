0086966d6ab074b3dcd4f8abf90e7c815d47be0a
Lukie Kang <lkang@bink.com>
2023-05-31 22:38:16 +0100

    feat: placeholder header component

---
+++ src/app/games/[id]/GameContainer/Header/Header.tsx
@@ -0,0 +1,21 @@
+'use client';
+import {Heading1, Heading2, Paragraph} from "@/components/Text/Text"
+import {Game, Player} from "@/types"
+
+export default function Game({game, players}: {game: Game, players: Player[] }) {
+  const { name, admin} = game || {name: '', admin: ''}
+
+  const renderPlayers = () => {
+    return players.map((player, index) => {
+      return <span key={player.id}>{index !== 0 && ','} {player.name} </span>
+    })
+  }
+
+  return (
+      <section>
+      <Heading1>{name}</Heading1>
+      <Heading2>Created By {admin}</Heading2>
+        <Paragraph>Players:{renderPlayers()} </Paragraph>
+      </section>
+  )
+}
