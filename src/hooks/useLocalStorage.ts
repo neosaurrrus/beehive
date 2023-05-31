b8e3fe8bff2586124ba811b59a5bd4916bc320fa
Lukie Kang <lkang@bink.com>
2023-05-31 22:34:18 +0100

    feat: add future useLocalStorage Hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,17 @@
+import { useState, useCallback } from 'react';
+
+function useLocalStorage(key:string, initialState:string) {
+  const [value, setValue] = useState(localStorage.getItem(key) ?? initialState);
+  const updatedSetValue = useCallback(
+    newValue => {
+      if (newValue === initialState || typeof newValue === 'undefined') {
+        localStorage.removeItem(key);
+      } else {
+        localStorage.setItem(key, newValue);
+      }
+      setValue(newValue ?? initialState);
+    },
+    [initialState, key]
+  );
+  return [value, updatedSetValue];
+}
