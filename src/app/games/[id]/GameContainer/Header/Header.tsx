d402dfbdc3524a655dc9e6c425bf6542492025c0
Lukie Kang <lkang@bink.com>
2023-06-02 00:53:18 +0100

    feat: add styling for header

--- src/app/games/[id]/GameContainer/Header/Header.tsx
+++ src/app/games/[id]/GameContainer/Header/Header.tsx
@@ -3,12 +3,12 @@ import { supabase } from '@/lib/supabaseClient'
 import {Heading1, Heading2, Paragraph} from "@/components/Text/Text"
 import {Game, Player} from "@/types"
 
-export default function Header({game, players = []}: {game: Game, players: Player[] }) {
+export default function Header({game, players, currentPlayer}: {game: Game, players: Player[], currentPlayer: Player }) {
   const { name, admin} = game || {name: '', admin: ''}
 
   const renderPlayers = () => {
     return players.map((player, index) => {
-      return <span key={player.id}>{index !== 0 && ','} {player.name} </span>
+      return <span className='text-accent' key={player.id}>{index !== 0 && ','} {player.name} </span>
     })
   }
 
@@ -20,7 +20,6 @@ export default function Header({game, players = []}: {game: Game, players: Playe
       error && console.log(error)
   }
 
- 
   const resetPlayerScores = async () => {
     const { error, data } = await supabase
       .from('players')
@@ -29,28 +28,36 @@ export default function Header({game, players = []}: {game: Game, players: Playe
 
       error && console.log(error)
 
-
       // TODO: set game state to not revealed
   }
 
-  return (
-      <section>
-      <Heading1>{name}</Heading1>
-      <Heading2>Created By {admin}</Heading2>
-        <Paragraph>Players:{renderPlayers()} </Paragraph>
-      <Paragraph>Is revealed {game.is_revealed.toString()}</Paragraph>
-      
+  const renderAdminButtons = () => (
+    <div className='flex flex-col gap-4 justify-center items-center'>
       <button 
-        className="bg-green-400 text-white p-4 rounded-lg shadow-lg"
+        className={buttonClasses}
         onClick={revealPlayerScores}
         >{game.is_revealed ? 'Hide' : 'Reveal'}
       </button>
 
       <button
-        className="bg-red-400 text-white p-4 rounded-lg shadow-lg"
+        className={buttonClasses}
         onClick={resetPlayerScores}
         >Reset
       </button>
+    </div>
+  )
+  
+  const buttonClasses='bg-accent text-darkText text-sm font-semibold tracking-wide p-4 rounded-4xl shadow-lg w-24 h-12 hover:bg-accent/75 duration-200'
+
+  return (
+      <section className='bg-sectionBackground rounded-3xl p-8 h-48 shadow-xl flex'>
+        <div className='w-4/5 flex flex-col justify-center'>
+          <Heading1>{name}</Heading1>
+          <Heading2 extraClasses='text-lightText mt-2 mb-6'>Created by <span className='text-accent font-semibold'>{admin}</span></Heading2>
+          <Paragraph extraClasses='font-semibold'>Participants:{renderPlayers()} </Paragraph>
+        </div>
+
+       {currentPlayer.is_admin && renderAdminButtons()}
       </section>
   )
 }
