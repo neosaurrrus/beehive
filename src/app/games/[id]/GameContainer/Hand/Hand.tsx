c72e0088b64b4471151e5da268e2621586d0389a
Lukie Kang <lkang@bink.com>
2023-06-02 00:53:43 +0100

    feat: add styling for hand and handcard

--- src/app/games/[id]/GameContainer/Hand/Hand.tsx
+++ src/app/games/[id]/GameContainer/Hand/Hand.tsx
@@ -1,19 +1,21 @@
 'use client';
 import {Player} from "@/types"
 import HandCard from './HandCard/HandCard';
-import { useState } from "react";
+import { Space_Grotesk } from 'next/font/google';
+const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
 
 const cardsToUse = [0,1,2,3,5,8,13,20,40]
 export default function Hand({currentPlayer}: {currentPlayer: Player}) {
 
 
+  const renderCards = () => {
+    return cardsToUse.map((cardScore, index) => {
+      return <HandCard key={index} score={cardScore} playerId={currentPlayer.id}/>
+    })
+  }
   return (
-      <section>
-        {!currentPlayer.score && cardsToUse.map((cardScore, index) => {
-          return <HandCard key={index} score={cardScore} playerId={currentPlayer.id}/>
-        })}
-      
-      <p>Player: {currentPlayer.id}- {currentPlayer.name} - { currentPlayer.score}</p>
-      </section>
+    <section className='bg-sectionBackground rounded-3xl p-8 h-36 shadow-xl flex justify-center items-center'>
+       {currentPlayer.score ? <p className={`text-4xl text-accent font-semibold  ${spaceGrotesk.className}`}>You played {currentPlayer.score}, waiting for others...</p> : renderCards()}
+    </section>
   )
 }
--- src/app/games/[id]/GameContainer/Hand/HandCard/HandCard.tsx
+++ src/app/games/[id]/GameContainer/Hand/HandCard/HandCard.tsx
@@ -1,25 +1,21 @@
 
 import { supabase } from '@/lib/supabaseClient'
-import {Heading1, Heading2, Paragraph} from "@/components/Text/Text"
-
+import { Space_Grotesk } from 'next/font/google';
+const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
 
 export default function HandCard({score, playerId = 0}: {score: number , playerId: number}) {
-
   const handlePlayerScore = async () => {
-    const { error, data: playerData } = await supabase
+    const { error } = await supabase
       .from('players')
       .update ({ score: score })
       .eq('id', playerId)
-      .select()
 
       error && console.log(error)
-
-      console.log(playerData)
   }
-    
+
   return (
-      <button className="bg-gray-500 rounded-lg shadow-lg p-4 m-2" onClick={handlePlayerScore}>
-        <p className="text-2xl">{score}</p>
+      <button className={`bg-accent text-darkText text-4xl text-center font-medium w-20 h-20 rounded-lg shadow-xl m-2 p-2 hover:bg-accent/75 duration-200 ${spaceGrotesk.className}`} onClick={handlePlayerScore}>
+        {score}
       </button>
   )
 }
