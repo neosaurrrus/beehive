3be0eb6333fc411ce579463ee4ba79ac72032ae8
Lukie Kang <lkang@bink.com>
2023-06-02 10:41:26 +0100

    fix: bad escape char

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
      <section className='text-center flex flex-col gap-8 w-4/5'>
-      <Heading1>Lukie`&apos`s Planning Poker of Mystery</Heading1>
+      <Heading1>Lukies Planning Poker of Mystery</Heading1>
       <Paragraph>Planning poker®, also called Scrum poker, is a consensus-based technique for estimating, mostly used to estimate effort or relative size of user stories in software development. In planning poker, members of the group make estimates by playing numbered cards face-down to the table, instead of speaking them aloud. The cards are revealed, and the estimates are then discussed. By hiding the figures in this way, the group can avoid the cognitive bias of anchoring, where the first number spoken aloud sets a precedent for subsequent estimates.</Paragraph>
       <Paragraph>To read more, check out the Wikipedia page.</Paragraph>
       <Link href="/new">
