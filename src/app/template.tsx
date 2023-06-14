4220f6ce3f3a4ef0bf6dc5c7d966b8bc8bd6c80e
Lukie Kang <lukiek@gmail.com>
2023-06-15 00:20:36 +0100

    feat: add initial simple page transition

---
+++ src/app/template.tsx
@@ -0,0 +1,24 @@
+'use client';
+// This performs a page transition to the new URL. Temporarty due to next js app folder issue https://github.com/framer/motion/issues/1850
+
+import { useEffect, useState } from 'react';
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const [fade, setFade] = useState(false);
+  useEffect(() => {
+    setFade(true);
+    return () => {
+      setFade(false);
+    };
+  }, []);
+  return (
+    <div
+       className={`
+        ${fade ? 'translate-y-0 opacity-100' : 'translate-y-3 opacity-0'} 
+        'transition duration-500 ease-out flex flex-col items-center justify-center min-h-screen'
+       `} 
+    >
+      {children}
+    </div>
+  );
+}
