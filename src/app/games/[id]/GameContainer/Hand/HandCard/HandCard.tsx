7f2acfba88a909db0fb053699d5dbc8e6bb6bbbe
Lukie Kang <lkang@bink.com>
2023-06-16 22:38:32 +0100

    feat: add small initial entry animation

--- src/app/games/[id]/GameContainer/Hand/HandCard/HandCard.tsx
+++ src/app/games/[id]/GameContainer/Hand/HandCard/HandCard.tsx
@@ -1,7 +1,7 @@
 
 import { supabase } from '@/lib/supabaseClient'
-import { motion } from 'framer-motion';
-import { Space_Grotesk } from 'next/font/google';
+import { motion } from 'framer-motion'
+import { Space_Grotesk } from 'next/font/google'
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
 
 export default function HandCard({ score, playerId = 0 }: { score: number, playerId: number }) {
@@ -16,8 +16,8 @@ export default function HandCard({ score, playerId = 0 }: { score: number, playe
 
   return (
     <motion.button
-      className={`bg-accent text-darkText text-4xl text-center font-medium w-20 h-20 rounded-lg shadow-xl m-2 p-2 hover:bg-accent/75 duration-200 ${spaceGrotesk.className}`}
-      initial={{ scale: 1.5, opacity: 0, rotate: 45, translateY: -50, translateX: -25 }}
+      className={`bg-accent/75 text-darkText text-4xl text-center font-medium w-20 h-20 rounded-lg shadow-lg m-2 p-2 hover:bg-accent duration-200 ${spaceGrotesk.className}`}
+      initial={{ scale: 0.1, opacity: 0, rotate: 20, translateY: 0, translateX: -25 }}
       animate={{ rotate: 0, opacity: 1, scale: 1, translateY: 0, translateX: 0 }}
       whileHover={{ scale: 1.2 }}
       transition={{ duration: 0.2 }}
