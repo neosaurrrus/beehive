7e93f7070d54fd05262f96d7819ee686cb63b8bb
Lukie Kang <lukiek@gmail.com>
2023-06-20 23:30:25 +0100

    fix: add missing eslint config item

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    project: ["./tsconfig.json"],
   },
   rules: {
     "prettier/prettier": "error",
